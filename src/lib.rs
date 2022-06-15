14b686a11b9a2fec326bc234f7ded095c331bb25
Matej Bačo <matejbaco2000@gmail.com>
2022-06-15 14:39:09 +0000

    Fix stringify method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
 use ext_php_rs::prelude::*;
 use scrypt::{scrypt, Params};
-use std::str;
 
 #[php_function]
 pub fn rust_scrypt(password: &str, salt: &str, log_n: u8, r: u32, p: u32, length: usize) -> String {
@@ -9,9 +8,8 @@ pub fn rust_scrypt(password: &str, salt: &str, log_n: u8, r: u32, p: u32, length
 
     let params = Params::new(log_n, r, p).unwrap();
     scrypt(password.as_bytes(), salt.as_bytes(), &params, &mut result).unwrap();
-    let hash = str::from_utf8(&result).unwrap();
     
-    format!("{}", hash)
+    format!("{}", String::from_utf8_lossy(result.as_slice()))
 }
 
 #[php_module]
